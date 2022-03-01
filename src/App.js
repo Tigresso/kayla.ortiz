a01c4b7c759a860c111f8b98086e81ca455bca96
Tigresso <kayla10463@yahoo.com>
2022-02-28 21:46:55 -0500

    2 more photos added next to logo,link+text color changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from '/Users/kaylaortiz/Desktop/kayla.ortiz.github.io/src/kayla ortiz.jpg';
+import logo2 from '/Users/kaylaortiz/Desktop/kayla.ortiz.github.io/src/Tigress.JPG';
+import logo3 from '/Users/kaylaortiz/Desktop/kayla.ortiz.github.io/src/art.jpg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo2" alt="logo2" />
+        <img src={logo3} className="App-logo3" alt="logo3" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo2" alt="logo2" /> */}
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
           My name is Kayla and I'm going to master React!
